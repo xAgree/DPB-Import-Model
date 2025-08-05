7f422f8e322f39beaf7a3c5b8796605b543ebfe6
xAgree <contact.ethanyoung@gmail.com>
2025-08-05 16:02:48 +1200

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def process_files(uploaded_files, filters, comment_filter):
 
     # Apply filters
     if comment_filter:
+        pattern = '|'.join(comment_filter)
         combined_df = combined_df[~combined_df['Comment'].str.contains(comment_filter, na=False)]
 
     if filters:
